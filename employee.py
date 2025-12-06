a8640587dd26c28e6f0bec2910176e0e0bdc7ec8
ruman-jagirdar <rumanjagirdar08@gmail.com>
2025-12-06 12:02:55 +0530

    Create employee.py

---
+++ employee.py
@@ -0,0 +1,14 @@
+def employee_details(name,emp_id,department,salary):
+    result=(
+      f"Empployee Name:{name}\n"
+      f"Employee ID:{emp_id}\n"
+      f"Department:{department}\n"
+      f"Salary:{salary}\n"
+    )
+  return result
+if__name__=="__main__":
+   name="RUMAN"
+   emp_id="E1001"
+   department="IT"
+   salary=50000
+   print(employee_details(name,emp_id,department,salary))
